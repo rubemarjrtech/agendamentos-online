536bfd427217501dbea7674d2bdba790c697e344
Rubemar Junior <rubemarrocha22@gmail.com>
2026-07-25 11:37:40 -0300

    fix: ajustar a config de tokens

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -34,7 +34,7 @@ export class AuthController {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       maxAge: 24 * 60 * 60 * 1000,
-      sameSite: 'lax',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       path: '/',
     });
 
@@ -58,7 +58,7 @@ export class AuthController {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       maxAge: 24 * 60 * 60 * 1000,
-      sameSite: 'lax',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       path: '/',
     });
 
@@ -82,7 +82,7 @@ export class AuthController {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       maxAge: 24 * 60 * 60 * 1000,
-      sameSite: 'lax',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       path: '/',
     });
 
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,11 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@app/app.module';
 import appConfig from '@app/config/app.config';
+import type { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const nestApp = await NestFactory.create(AppModule);
+  const nestApp = await NestFactory.create<NestExpressApplication>(AppModule);
+  nestApp.set('trust proxy', 1);
   const app = appConfig(nestApp);
 
   await app.listen(process.env.PORT ?? 3000);
